0fff88c9d03c608e0051030cc65b8cc444e0fb38
NenoAlmeida <nenoxvx@icloud.com>
2016-05-09 12:08:08 -0300

    Add nre files and edit / destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
@@ -7,7 +9,6 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def create
@@ -20,7 +21,27 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
   private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :content)
